e1dcd6e2a27a66334b9f7b936b85f509350969e2
raashed <mohammedraashed10@gmail.com>
2026-03-07 15:30:07 +0530

    feat(validation): trade-validation was introduced

--- trpc-server/src/validators/index.ts
+++ trpc-server/src/validators/index.ts
@@ -1,5 +1,37 @@
 import { z } from "zod";
 
+// trade-procedure validation
+export type TradeAsset = z.infer<typeof tradeSchema>["asset"];
+
+export const tradeOpenOutputSchema = z.object({
+    orderId: z.uuid()
+}).strict();
+
+export const tradeCloseInputSchema = z.object({
+    orderId: z.uuid()
+}).strict();
+
+export const tradeCloseOutputSchema = z.object({
+    pnl: z.number(),
+    message: z.string()
+}).strict();
+
+export const tradeSchema = z.object({
+    type: z.enum(["buy", "sell"]),
+    margin: z.number().positive(),
+    leverage: z.union([
+        z.literal(1),
+        z.literal(5),
+        z.literal(10),
+        z.literal(20),
+        z.literal(100),
+    ]),
+    asset: z.enum(["BTC", "ETH", "SOL"]),
+    takeProfit: z.number().positive().optional(),
+    stopLoss: z.number().positive().optional(),
+});
+
+// trades-procedure validation
 const tradesOutputSchema = z.object({
     orderId: z.string().min(1),
     type: z.enum(["buy", "sell"]),
@@ -35,6 +67,7 @@ export const orderSchema = {
     }).strict()
 }
 
+// user-procedure validation
 export const authSchema = {
     input: z.object({
         email: z.email( { message: "invalid email for sign-up procedure" } ),
