ec14811a492745e0478587a17c4c663966d7af93
raashed <mohammedraashed10@gmail.com>
2026-03-07 14:18:09 +0530

    refactor(validation): added zod validation to the procedures

--- rpc-server/src/routes/v1/user.ts
+++ rpc-server/src/routes/v1/user.ts
@@ -1,13 +1,14 @@
 import { publicProcedure, router } from "../../trpc";
+import { authSchema } from "../../validators";
 
 export const userRouter = router({
     signup: publicProcedure
-    .input()
-    .output()
+    .input(authSchema.input)
+    .output(authSchema.output)
     .mutation(),
 
     signin: publicProcedure
-    .input()
-    .output()
+    .input(authSchema.input)
+    .output(authSchema.signinOutput)
     .mutation()
 })
