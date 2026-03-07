34d516f75a8eefb4c8990988d4f5710fd9bc6025
raashed <mohammedraashed10@gmail.com>
2026-03-07 16:47:17 +0530

    chore(bin): dev script was introduced

--- trpc-server/src/bin.ts
+++ trpc-server/src/bin.ts
@@ -2,5 +2,5 @@ import { app } from ".";
 import { BACKEND_PORT } from "./config/config";
 
 app.listen(BACKEND_PORT, () => {
-    console.log(`your rpc-server is listening on http://localhost:${BACKEND_PORT}`)
+    console.log(`your trpc-server is listening on http://localhost:${BACKEND_PORT}`)
 })
