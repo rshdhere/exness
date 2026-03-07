84a6f3891a7eef726f509ddac90bb79ce80200a9
raashed <mohammedraashed10@gmail.com>
2026-03-07 14:38:44 +0530

    feat(sign-in): new tRPC-procedure was introduced

--- rpc-server/src/routes/v1/user.ts
+++ rpc-server/src/routes/v1/user.ts
@@ -1,10 +1,11 @@
 import { v5 } from "uuid";
 import { publicProcedure, router } from "../../trpc";
 import { authSchema } from "../../validators";
-import { PLACEHOLDER } from "../../config/config";
+import { JWT_SECRET, PLACEHOLDER } from "../../config/config";
 import { USERS } from "../../data/data";
 import { TRPCError } from "@trpc/server";
 import { toInternalUSD } from "../../utils/utils";
+import jwt from "jsonwebtoken";
 
 export const userRouter = router({
     signup: publicProcedure
@@ -37,5 +38,38 @@ export const userRouter = router({
     signin: publicProcedure
     .input(authSchema.input)
     .output(authSchema.signinOutput)
-    .mutation()
+    .mutation(( { input } ) => {
+        const uuid = v5(input.email, PLACEHOLDER);
+
+        if (!USERS[uuid]){
+            throw new TRPCError({
+                code: "NOT_FOUND",
+                message: "user was not found in-memory"
+            })
+        };
+
+        if (USERS[uuid].password !== input.password) {
+            throw new TRPCError({
+                code: "UNAUTHORIZED",
+                message: "password do not match"
+            })
+        };
+
+        if (!JWT_SECRET) {
+            throw new TRPCError({
+                code: "INTERNAL_SERVER_ERROR",
+                message: "JWT_SECRET went missing"
+            })
+        };
+
+        const token = jwt.sign({
+            userId: uuid
+        }, JWT_SECRET, {
+            expiresIn: "1h"
+        });
+
+        return {
+            token
+        }
+    })
 })
