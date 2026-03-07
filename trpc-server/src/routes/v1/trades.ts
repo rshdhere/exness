04527da303bfa616d9d04e9e9a330d2d5e34f38e
raashed <mohammedraashed10@gmail.com>
2026-03-07 15:11:17 +0530

    feat(trades.open): new tRPC procedure was introduced

--- trpc-server/src/routes/v1/trades.ts
+++ trpc-server/src/routes/v1/trades.ts
@@ -1,12 +1,42 @@
+import { ORDERS } from "../../data/data";
 import { privateProcedure, router } from "../../trpc";
+import { orderSchema } from "../../validators";
 
 export const tradesRouter = router({
     open: privateProcedure
-    .input()
-    .output()
-    .mutation(),
+    .input(orderSchema.openInput)
+    .output(orderSchema.openOutput)
+    .mutation( ({ ctx }) => {
+        const userId = ctx.userId;
+
+        if (!ORDERS[userId]){
+            return {
+                trades: []
+            }
+        };
+
+        const formattedTrades = Object.entries(ORDERS[userId]).map(
+            ([orderId, order]) => ({
+                orderId,
+                type: order.type,
+                margin: order.margin,
+                leverage: order.leverage,
+                asset: order.asset,
+                openPrice: order.openPrice,
+                takeProfit: order.takeProfit,
+                stopLoss: order.stopLoss,
+                liquidationPrice: order.liquidationPrice,
+            })
+        );
+
+        return {
+            trades: formattedTrades
+        }
+
+
+    } ),
 
     getAll: privateProcedure
-    .output()
+    .output(orderSchema.getAllOutput)
     .query()
 })
