041b98fe7cf4e79d0e8b14e3a01c300b5f9598e1
raashed <mohammedraashed10@gmail.com>
2026-03-07 15:03:43 +0530

    feat(validation): trades-validation was introduced

--- trpc-server/src/validators/index.ts
+++ trpc-server/src/validators/index.ts
@@ -1,5 +1,40 @@
 import { z } from "zod";
 
+const tradesOutputSchema = z.object({
+    orderId: z.string().min(1),
+    type: z.enum(["buy", "sell"]),
+    margin: z.number().positive(),
+    leverage: z.number().positive(),
+    asset: z.string().min(1),
+    openPrice: z.number().positive(),
+    takeProfit: z.number().positive().optional(),
+    stopLoss: z.number().positive().optional(),
+    liquidationPrice: z.number().positive().optional()
+}).strict();
+
+const closedTradesOutputSchema = z.object({
+    orderId: z.string().min(1),
+    type: z.enum(["buy", "sell"]),
+    margin: z.number().positive(),
+    leverage: z.number().positive(),
+    openPrice: z.number().positive(),
+    closePrice: z.number().positive(),
+    pnl: z.number()
+}).strict();
+
+export const orderSchema = {
+    openInput: z.void(),
+    getAllInput: z.void(),
+    trade: tradesOutputSchema,
+    closedTrade: closedTradesOutputSchema,
+    openOutput: z.object({
+        trades: z.array(tradesOutputSchema)
+    }).strict(),
+    getAllOutput: z.object({
+        trades: z.array(closedTradesOutputSchema)
+    }).strict()
+}
+
 export const authSchema = {
     input: z.object({
         email: z.email( { message: "invalid email for sign-up procedure" } ),
