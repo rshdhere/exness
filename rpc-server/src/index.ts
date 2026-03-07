6062a5806ab468b796c1cfb41842bb2591162ad1
raashed <mohammedraashed10@gmail.com>
2026-03-07 13:57:18 +0530

    feat(express): init express

--- rpc-server/src/index.ts
+++ rpc-server/src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import express from "express";
+import { userRouter } from "./routes/v1/user";
+import { router } from "./trpc";
+
+export const appRouter = router({
+    v1: router({
+        user: userRouter
+    })
+});
+
+export type AppRouter = typeof appRouter;
+
+const app = express();
+
+app.use(express.json());
