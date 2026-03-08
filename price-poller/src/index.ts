27d639da5ad2f00305d20714ae83d7394cc6b86c
raashed <mohammedraashed10@gmail.com>
2026-03-08 07:51:04 +0530

    feat(redis): redis-client was initialized

--- price-poller/src/index.ts
+++ price-poller/src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import { createClient } from "redis";
+
+async function main(){
+    const redis = await createClient({
+        url: "redis://redis_service:6379"
+    }).connect();
+}
+
+main();
