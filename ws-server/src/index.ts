b9a69ff551162da9f7d3a0a9d6fca2c0341118c9
raashed <mohammedraashed10@gmail.com>
2026-03-08 07:01:50 +0530

    fix(ws-server): fix Redis fan-out iteration for subscribed sockets

--- ws-server/src/index.ts
+++ ws-server/src/index.ts
@@ -14,6 +14,43 @@ const client = new Map<WebSocket, Set<string>>();
 export const Channels = ['SOL', 'ETH', 'BTC'];
 
 const start = async () => {
+    await redis.connect();
+
+    Channels.forEach((ch) => {
+        redis.subscribe(ch, (data) => {
+            client.forEach((symbs, ws: WebSocket) => {
+                if (symbs.has(ch)) {
+                    ws.send(data);
+                }
+            });
+        });
+    });
+
+    websocket.on("connection", (ws: WebSocket) => {
+        
+        client.set(ws, new Set());
+
+        ws.on("message", (msg) => {
+            const message = JSON.parse(msg.toString());
+
+            if (message.type === "SUBSCRIBE"){
+                if (!client.has(ws)) {
+                    client.set(ws, new Set());
+                };
+                const symbs = client.get(ws);
+                symbs?.add(message.symbol)
+            };
+
+            if (message.type === "UNSUBSCRIBE"){
+                const symbs = client.get(ws);
+                symbs?.delete(message.symbol);
+
+                if (symbs?.size === 0){
+                    client.delete(ws)
+                }
+            }
+        })
+    })
 
 };
 
